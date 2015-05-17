3b7ce83fc3f8c64a95fbd053618880bbf53dba5c
Kyle Benson <kyle.edward.benson@gmail.com>
2015-05-17 16:43:40 -0700

    fixed bug regarding shortest latency path

--- src/geocron/model/gsford-ron-path-heuristic.cc
+++ src/geocron/model/gsford-ron-path-heuristic.cc
@@ -81,7 +81,7 @@ GsfordRonPathHeuristic::GetLikelihood (Ptr<RonPath> path)
       Time thisLatency = physPath->GetLatency ();
       // Need to do 1/latency so that low latency paths get higher likelihood
       uint64_t millis = thisLatency.GetMilliSeconds ();
-      if (millis != 0)
+      if (millis <= 0)
       {
           NS_LOG_DEBUG ("Link with 0ms latency, this will cause a divide by zero error so setting to 1!");
           millis = 1;
